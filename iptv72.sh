f0a9eda0a7d9eb65922028f2b097dec4185dbd80
ArronYin <diskerjtr@qq.com>
2017-01-20 14:39:00 +0800

    iptv72.sh

---
+++ iptv72.sh
@@ -0,0 +1,63 @@
+#!/bin/sh
+
+#echo 欢迎使用本脚本
+ping 127.0.0.1 -c 2 > /dev/null
+echo 本脚本只适用于merlin 7.2改版固件，其他固件请勿运行本脚本
+ping 127.0.0.1 -c 2 > /dev/null
+echo 仅支持7.1及以上固件
+ping 127.0.0.1 -c 2 > /dev/null
+echo 本提示保留5秒，如果您不是merlin改版固件请立刻按下Ctrl+C中止本脚本
+ping 127.0.0.1 -c 10 > /dev/null
+echo 本脚本适用于上海电信需要AB面认证的IPTV，其他地区请按实际情况修改脚本
+ping 127.0.0.1 -c 2 > /dev/null
+echo 使用前请确认光猫已经设置桥接
+ping 127.0.0.1 -c 2 > /dev/null
+echo 使用脚本前确认SS是工作状态或者是关闭状态，否则会导致脚本运行失败
+ping 127.0.0.1 -c 2 > /dev/null
+
+#空行
+echo
+
+
+#脚本提示
+echo 正在修改ss dnsmasq配置文件
+#移动到ss dnsmasq目录
+cd /koolshare/ss/rules
+#删除旧配置文件
+echo 正在删除旧文件
+rm -rf dnsmasq.postconf
+#下载新配置文件
+echo 正在下载新文件
+wget -q --no-check-certificate https://raw.githubusercontent.com/ArronYin/4K-IPTV/master/dnsmasq.conf
+#重命名新配置文件
+mv dnsmasq.conf dnsmasq.postconf
+#设置权限
+chmod -R 0755 dnsmasq.postconf
+#完成提示
+echo 成功
+
+#延迟运行
+ping 127.0.0.1 -c 2 > /dev/null
+#空行
+echo
+
+#运行提示
+echo 正在重启dnsmasq
+#重启dnsmasq
+service restart_dnsmasq > /dev/null
+#完成提示
+echo dnsmasq已重启
+
+#延迟运行
+ping 127.0.0.1 -c 2 > /dev/null
+#空行
+echo
+
+#返回默认目录
+cd
+
+#提示成功
+ping 127.0.0.1 -c 2 > /dev/null
+echo 脚本运行完成，如果光猫已经设置完毕，您可以把IPTV接在Lan1-4任意接口
+ping 127.0.0.1 -c 2 > /dev/null
+echo 感谢您的使用,作者ArronYin
