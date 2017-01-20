1485b8344b2637083851eacc5fe75c53e85a4d43
ArronYin <diskerjtr@qq.com>
2017-01-20 20:43:40 +0800

    Update SHiptv.sh

--- SHiptv.sh
+++ SHiptv.sh
@@ -4,24 +4,23 @@
 ping 127.0.0.1 -c 2 > /dev/null
 echo 本脚本只适用于merlin改版固件，其他固件请勿运行本脚本
 ping 127.0.0.1 -c 2 > /dev/null
-echo 本提示保留5秒，如果您不是merlin改版固件请立刻按下Ctrl+C中止本脚本
+echo 本提示保留10秒，如果您不是merlin改版固件请立刻按下Ctrl+C中止本脚本
 ping 127.0.0.1 -c 10 > /dev/null
 echo 本脚本适用于上海电信需要AB面认证的IPTV，其他地区请按实际情况修改脚本
 ping 127.0.0.1 -c 2 > /dev/null
 echo 使用前请确认光猫已经设置桥接
 ping 127.0.0.1 -c 2 > /dev/null
-
 #空行
 echo
 
-#开始提示
-echo 正在增加IPTV开机自动启动脚本
+#脚本提示
+echo 正在增加IPTV开机自启动脚本
 #移动到脚本目录
 cd /jffs/scripts
 #删除之前的脚本
 echo 正在删除旧文件
 rm -rf wan-start*
-#下载开机运行脚本
+#下载自启动脚本
 echo 正在下载新文件
 wget -q --no-check-certificate https://raw.githubusercontent.com/ArronYin/4K-IPTV/master/wan-start
 #设置权限
